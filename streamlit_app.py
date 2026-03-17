f0b23732a14fb9c1be5b27d58fc24cc5e70b82b2
jaxobrien <jax.a.obrien@gmail.com>
2026-03-17 15:24:04 +0100

    updates 17 march

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-from charts.happiness_by_year import plot_happiness_by_year
+from utils.charts.happiness_by_year import plot_happiness_by_year
 
 # Load data
-df = pd.read_csv("indexed_data_all_obs.csv", low_memory=False)
+df = pd.read_csv("utils/indexed_data_all_obs.csv", low_memory=False)
 
 # App title
 st.title("Exploring the relationship between social media use and children's happiness")
